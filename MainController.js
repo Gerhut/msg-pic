5b6a9e95cb3e75c74de63fe16183c6189a5c624e
George Chung <Gerhut@GMail.com>
2013-12-10 15:37:18 +0800

    改用Bing Search API

--- MainController.js
+++ MainController.js
@@ -1,17 +1,22 @@
 angular.module('app', []).controller('MainController', ['$scope', '$http', function ($scope, $http) {
   $scope.images = [];
   $scope.updateImage = function () {
-    console.log('change');
-    var url = 'http://image.baidu.com/i' + 
-      '?tn=baiduimagejson' +
-      '&word=' + $scope.message +
-      '&rn=200' +
-      '&callback=JSON_CALLBACK';
-    $http.jsonp(url).success(function (data) {
-      $scope.pictures = data.data.map(function (entry) {
+    var api = 'https://api.datamarket.azure.com/Bing/Search/Image'
+      , accountKey = 'qQCEetmiVEYsMnPVd6X5u4wzX6StpyWKWGu0e3lWqFM'
+      , authorization = 'Basic ' + btoa(accountKey + ':' + accountKey);
+    $http.get(api, {
+      params: {
+        '$format': 'json',
+        'Query': "'" + $scope.message + "'"
+      },
+      headers: {
+        'Authorization': authorization
+      }
+    }).success(function (data) {
+      $scope.pictures = data.d.results.map(function (entry) {
         return {
-          'thumb': entry.objURL,
-          'source': entry.objURL
+          'thumb': entry.Thumbnail.MediaUrl,
+          'source': entry.MediaUrl
         };
       });
     });
